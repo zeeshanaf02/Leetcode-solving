e11b0bd3c6d348aa43e5cd6849105594fb45a051
Mohammed Zeeshan <143378963+zeeshanaf02@users.noreply.github.com>
2024-11-13 19:33:04 +0530

    Create 2563.py

---
+++ 2563.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        nums.sort()
+
+        def countLess(summ: int) -> int:
+            res = 0
+            i = 0
+            j = len(nums) - 1
+            while i < j:
+                while i < j and nums[i] + nums[j] > summ:
+                    j -= 1
+                res += j - i
+                i += 1
+            return res
+
+        return countLess(upper) - countLess(lower - 1)
